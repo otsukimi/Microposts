f60fdfdfc6e0e22628a8ddd12c72bd0ccd12da84
otsukimi.W <w_otsukimi@yahoo.co.jp>
2019-11-15 07:03:37 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
   def index
     @users = User.all.page(palams[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
   end
 
   def new
